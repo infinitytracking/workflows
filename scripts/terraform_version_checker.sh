2512385961271fe6cc4b68e3d14bcdc8effd200b
James Cradock <james.cradock@infinity.co>
2022-03-01 14:18:01 +0000

    Add version from releases

--- scripts/terraform_version_checker.sh
+++ scripts/terraform_version_checker.sh
@@ -2,7 +2,7 @@
 
 EXISTING_VERSION=$(yq '.on.workflow_call.inputs.terraform_version.default' .github/workflows/terraform.yml)
 
-LATEST_VERSION='v1.1.6'
+LATEST_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | jq -r .tag_name)
 LATEST_VERSION=${LATEST_VERSION//v}
 
 if [ "$EXISTING_VERSION" != "$LATEST_VERSION" ]; then
