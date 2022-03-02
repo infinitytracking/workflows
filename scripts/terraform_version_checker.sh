efaf1e8f3bb0132b4ac614fae5119ef40830e660
James Cradock <james.cradock@infinity.co>
2022-03-02 10:16:20 +0000

    Add PR creation

--- scripts/terraform_version_checker.sh
+++ scripts/terraform_version_checker.sh
@@ -2,11 +2,19 @@
 
 EXISTING_VERSION=$(yq '.on.workflow_call.inputs.terraform_version.default' .github/workflows/terraform.yml)
 
-LATEST_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | jq -r .tag_name)
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest)
+LATEST_VERSION=$(echo "$LATEST_RELEASE" | tr '\r\n' ' ' | jq -r .tag_name)
 LATEST_VERSION=${LATEST_VERSION//v}
 
 if [ "$EXISTING_VERSION" != "$LATEST_VERSION" ]; then
   yq e ".on.workflow_call.inputs.terraform_version.default=\"$LATEST_VERSION\"" .github/workflows/terraform.yml > /tmp/terraform.yml.tmp
   diff -U0 -w -b --ignore-blank-lines .github/workflows/terraform.yml /tmp/terraform.yml.tmp > /tmp/terraform.diff
   patch -s .github/workflows/terraform.yml /tmp/terraform.diff
+  RELEASE=$(echo "$LATEST_RELEASE" | tr '\r\n' ' ' | jq -r .html_url)
+
+  if [[ -n "${GITHUB_ACTIONS}" ]]; then
+    echo "::set-output name=EXISTING_VERSION::$EXISTING_VERSION"
+    echo "::set-output name=LATEST_VERSION::$LATEST_VERSION"
+    echo "::set-output name=RELEASE::$RELEASE"
+  fi
 fi
